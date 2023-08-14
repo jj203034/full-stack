48f4f75a258a7d0300a611453082b134aa835b1a
jj203034 <121602898+jj203034@users.noreply.github.com>
2023-08-14 11:36:04 +0900

    Delete portfolio directory

--- portfolio/resources/scripts/portfolio.js
+++
@@ -1,8 +0,0 @@
-const link = document.querySelectorAll('.link');
-link.forEach(link => {
-    link.addEventListener('click', e => {
-        e.preventDefault();
-        const target = document.querySelector(link.getAttribute('href'));
-        target.scrollIntoView({behavior:'smooth'});
-    });
-});
